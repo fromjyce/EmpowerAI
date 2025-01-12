b00178230081c1463a7fb9a7043167e961d6fa73
Jayashre <jaya2004kra@gmail.com>
2025-01-12 23:54:06 +0530

    added user serial

---
+++ backend/api/serializers/user_serializer.py
@@ -0,0 +1,8 @@
+# backend/api/serializers/user_serializer.py
+from rest_framework import serializers
+from ..models.user_model import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
